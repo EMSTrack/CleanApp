7a5fc27189a39f5a2a9f263ec23dcb4c25aa2eb1
devin.hickey <dchickey@ucsd.edu>
2017-04-25 11:12:46 -0700

    Fixed Issue 11

---
+++ app/src/main/java/com/project/cruzroja/hospital/ToggleActivity.java
@@ -0,0 +1,18 @@
+package com.project.cruzroja.hospital;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by devinhickey on 4/25/17.
+ */
+
+public class ToggleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xray_availability);
+    }
+
+}
