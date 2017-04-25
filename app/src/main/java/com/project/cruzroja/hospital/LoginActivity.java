0b44adff6f0d3deffd7c800167bfca5f424f5a1f
devin.hickey <dchickey@ucsd.edu>
2017-04-25 11:35:51 -0700

    Fixed Issue 8

--- app/src/main/java/com/project/cruzroja/hospital/LoginActivity.java
+++ app/src/main/java/com/project/cruzroja/hospital/LoginActivity.java
@@ -3,7 +3,7 @@ package com.project.cruzroja.hospital;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
