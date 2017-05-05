4732ef07dfa140a3d51cbf328a711a5ca377e016
Fabian Choi <c.fbn@live.com>
2017-05-05 13:25:36 -0700

    Fixed Issue 25 Fixed Issue 26

--- app/src/main/java/com/project/cruzroja/hospital/LoginActivity.java
+++ app/src/main/java/com/project/cruzroja/hospital/LoginActivity.java
@@ -1,13 +1,26 @@
 package com.project.cruzroja.hospital;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        // Submit button's click listener
+        Button login = (Button) findViewById(R.id.submit_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent dashboard = new Intent(LoginActivity.this, DashboardActivity.class);
+                startActivity(dashboard);
+            }
+        });
     }
 }
