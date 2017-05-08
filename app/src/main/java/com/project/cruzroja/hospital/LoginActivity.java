441688aa5dfe40cdc8fb473fbc13f0ca9015e4e0
Daniel Martin <dmartinb@ucsd.edu>
2017-05-08 10:39:45 -0700

    Fix issue 27

--- app/src/main/java/com/project/cruzroja/hospital/LoginActivity.java
+++ app/src/main/java/com/project/cruzroja/hospital/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,14 +14,23 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        final EditText username_login = (EditText) findViewById(R.id.username);
+        final EditText password_login = (EditText) findViewById(R.id.password);
+
+
         // Submit button's click listener
-        Button login = (Button) findViewById(R.id.submit_login);
-        login.setOnClickListener(new View.OnClickListener() {
+        Button login_submit = (Button) findViewById(R.id.submit_login);
+        login_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent dashboard = new Intent(LoginActivity.this, DashboardActivity.class);
                 startActivity(dashboard);
+
+                String user_text = username_login.getText().toString();
+                String pass_text = password_login.getText().toString();
             }
         });
+
+
     }
 }
