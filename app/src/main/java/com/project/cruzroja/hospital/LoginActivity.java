ee17b80266df2649633bcf10e97d7965b355ed0e
Daniel Martin <dmartinb@ucsd.edu>
2017-05-08 17:34:05 -0700

    Fix issue 35

--- app/src/main/java/com/project/cruzroja/hospital/LoginActivity.java
+++ app/src/main/java/com/project/cruzroja/hospital/LoginActivity.java
@@ -3,7 +3,9 @@ package com.project.cruzroja.hospital;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -28,6 +30,15 @@ public class LoginActivity extends AppCompatActivity {
 
                 String user_text = username_login.getText().toString();
                 String pass_text = password_login.getText().toString();
+            }
+        });
+
+        findViewById(R.id.relativeLayout).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                return true;
             }
         });
 
