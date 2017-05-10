d0b966dc8f02dabe1689054fd77c9ad2fe5d8e03
Daniel Martin <dmartinb@ucsd.edu>
2017-05-10 13:44:25 -0700

    Fix issue 29

--- app/src/main/java/com/project/cruzroja/hospital/LoginActivity.java
+++ app/src/main/java/com/project/cruzroja/hospital/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.project.cruzroja.hospital;
 
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -9,15 +10,25 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.util.*;
+
 public class LoginActivity extends AppCompatActivity {
+    EditText username_login = (EditText) findViewById(R.id.username);
+    EditText password_login = (EditText) findViewById(R.id.password);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText username_login = (EditText) findViewById(R.id.username);
-        final EditText password_login = (EditText) findViewById(R.id.password);
+
 
 
         // Submit button's click listener
@@ -43,6 +54,53 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void loginHospital(){
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://cruzroja.ucsd.edu/auth/login";
+
+        // Request a string response from the provided URL.
+        StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // TODO: Get response and set permissions
+                System.out.println(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO: Error when login
+                System.out.println("Error on login");
+
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                String user_text = username_login.getText().toString();
+                String pass_text = password_login.getText().toString();
+                params.put("user",user_text);
+                params.put("pass",pass_text);
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Content-Type","application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+
+        // Add the request to the RequestQueue.
+        queue.add(sr);
+    }
+    public interface PostCommentResponseListener {
+        public void requestStarted();
+        public void requestCompleted();
+        public void requestEndedWithError(VolleyError error);
     }
 
 }
