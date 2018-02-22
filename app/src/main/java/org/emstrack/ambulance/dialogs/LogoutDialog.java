e62653887f6347b58498c96b945a0740a73322ac
Jessica Kwok <jcg.kwok@gmail.com>
2018-02-21 16:38:21 -0800

    adding activities to CleanApp

---
+++ app/src/main/java/org/emstrack/ambulance/dialogs/LogoutDialog.java
@@ -0,0 +1,76 @@
+package org.emstrack.ambulance.dialogs;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.emstrack.ambulance.AmbulanceApp;
+import org.emstrack.ambulance.LoginActivity;
+import org.emstrack.ambulance.R;
+import org.emstrack.mqtt.MqttProfileClient;
+
+/**
+ * Created by devinhickey on 5/24/17.
+ */
+
+public class LogoutDialog extends DialogFragment {
+
+    final String TAG = "LogoutDialog";
+
+    public LogoutDialog() {}
+
+    public static LogoutDialog newInstance() {
+        return new LogoutDialog();
+    }
+
+    @Override
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        System.out.println("Logout Dialog onCreateDialog");
+
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getActivity());
+
+        alertBuilder.setTitle(getResources().getString(R.string.alert_warning));
+        alertBuilder.setMessage(getResources().getString(R.string.logout_confirm));
+
+        // Create the OK button that logs user out
+        alertBuilder.setNeutralButton(getResources().getString(R.string.alert_button_positive_text), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println("OK Button Clicked");
+
+                // Retrieve client
+                final MqttProfileClient profileClient = ((AmbulanceApp) getActivity().getApplication()).getProfileClient();
+                try {
+                    profileClient.disconnect();
+                } catch (MqttException e) {
+                    Log.d(TAG,"Failed to disconnect.");
+                }
+
+                Intent rootIntent = new Intent(getActivity(), LoginActivity.class);
+                rootIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(rootIntent);
+
+            }
+        });
+
+        // Create the Cancel Button
+        alertBuilder.setNegativeButton(getResources().getString(R.string.alert_button_negative_text), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println("Cancel Button Clicked");
+                dialog.dismiss();
+            }
+        });
+
+        return alertBuilder.create();
+
+    }
+
+}
---
+++ app/src/main/java/org/emstrack/ambulance/interfaces/DataListener.java
@@ -0,0 +1,9 @@
+package org.emstrack.ambulance.interfaces;
+
+/**
+ * Created by Fabian Choi on 5/30/2017.
+ */
+
+public interface DataListener {
+    void onDataChanged(String name, String data);
+}
