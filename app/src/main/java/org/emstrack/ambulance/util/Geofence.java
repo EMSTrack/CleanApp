a5812dc079fe1eef157ed555e3a81bf382e77f31
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-11-28 18:06:48 -0800

    Updating structures for new call with waypoints.

--- app/src/main/java/org/emstrack/ambulance/util/Geofence.java
+++ app/src/main/java/org/emstrack/ambulance/util/Geofence.java
@@ -28,7 +28,7 @@ public class Geofence {
     }
 
     public Geofence(GPSLocation location, float radius, String type) {
-        this(new Waypoint(-1, false,
+        this(new Waypoint(-1, Waypoint.STATUS_NOT_VISITED,
                 new Location(null, type, location)), radius);
     }
 
