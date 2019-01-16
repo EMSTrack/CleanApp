41f809806ae6f31ad46d3b65f41ce4d5e7d35c0e
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-01-15 17:39:46 -0800

    Testing retrieving token with retrofit.

---
+++ models/src/main/java/org/emstrack/models/Token.java
@@ -0,0 +1,29 @@
+package org.emstrack.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Token {
+
+    @SerializedName("token")
+    @Expose
+    private String token;
+
+
+    /**
+     *
+     * @param token
+     */
+    public Token(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}
