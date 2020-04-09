90aa365fe92f49f058606f218937ec97ac61de84
James <jamesbasanmhs@gmail.com>
2020-04-09 15:16:13 -0700

    undo merge with master

--- app/src/main/java/org/emstrack/ambulance/fragments/EquipmentFragment.java
+++ app/src/main/java/org/emstrack/ambulance/fragments/EquipmentFragment.java
@@ -11,7 +11,6 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
